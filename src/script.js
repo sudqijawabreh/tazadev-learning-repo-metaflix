549bcb989239f6fecb6ef41a477f5d367835687a
Hazem Haddad <Hazem.Haddad@foothillsolutions.com>
2023-04-12 13:15:41 +0300

    Add animation to About us section showing

--- src/script.js
+++ src/script.js
@@ -1,6 +1,20 @@
 const mobileMenuIcon = document.querySelector('.mobile-menu-icon');
 const navbarMenu = document.querySelector('.navbar-menu');
+const aboutUs = document.querySelector('.about-us');
+const aboutUsOffset = aboutUs.offsetTop;
 
 mobileMenuIcon.addEventListener('click', () => {
   navbarMenu.classList.toggle('show');
 });
+
+window.addEventListener('scroll', () => {
+// Get the current scroll position
+const scrollPos = window.pageYOffset + window.innerHeight;
+
+// If the user has scrolled down to the about-us section, add the animate class
+if (scrollPos > aboutUsOffset + 100) {
+aboutUs.classList.add('animate');
+}
+});
+
+
